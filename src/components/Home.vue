9ca9d281507dd7d0b4495e0e5caad4676f140c29
rushjs1 <60147451+rushjs1@users.noreply.github.com>
2021-12-19 01:38:39 -0500

    update

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    Basic switch component
+    <div class="parent">
+      <SwitchBtn class="switch-btn" :value="false" v-model="switchBtnValue" />
+    </div>
+  </div>
+</template>
+
+<script>
+import SwitchBtn from "./customComponents/SwitchBtn";
+export default {
+  name: "Home",
+  components: {
+    SwitchBtn
+  },
+  watch: {
+    switchBtnValue() {
+      console.log(this.switchBtnValue);
+    }
+  },
+  data() {
+    return {
+      switchBtnValue: false
+    };
+  }
+};
+</script>
+
+<style>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+.parent {
+  margin-top: 3%;
+}
+</style>
---
+++ src/components/customComponents/SwitchBtn.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="switch">
+    <div
+      ref="switchBtn"
+      :class="!this.toggle ? 'switch-btn-false' : 'switch-btn-true'"
+      @click="changeValue"
+    >
+      <div :class="!this.toggle ? 'ball-false' : 'ball-true'"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SwitchBtn",
+  components: {},
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      toggle: this.value
+    };
+  },
+  methods: {
+    changeValue() {
+      this.toggle = !this.toggle;
+      this.$emit("input", this.toggle);
+    }
+  }
+};
+</script>
+
+<style>
+.switch-btn-false {
+  background-color: transparent;
+  border-radius: 7px;
+  width: 25px;
+  display: flex;
+  justify-content: flex-start;
+  transition: 0.3s;
+  border: 1px solid #c4c4c4;
+  height: 14px;
+  align-items: center;
+
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+}
+.ball-false {
+  background-color: #c4c4c4;
+  border-radius: 50px;
+  width: 10px;
+  height: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 1px;
+
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+}
+.switch-btn-true {
+  background-color: dodgerblue;
+  border-radius: 7px;
+  width: 25px;
+  display: flex;
+  justify-content: flex-end;
+  transition: 0.3s;
+  height: 14px;
+  align-items: center;
+  border: 2px solid rgb(30, 144, 255);
+  box-shadow: 0px 0px 0px 1.75px rgba(83, 169, 245, 0.7);
+
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+}
+
+.ball-true {
+  background-color: #ffffff;
+  border-radius: 50px;
+  width: 10px;
+  height: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 1px;
+
+  user-select: none; /* supported by Chrome and Opera */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+}
+
+.switch-btn-false:hover {
+  cursor: pointer;
+}
+.switch-btn-true:hover {
+  cursor: pointer;
+}
+</style>
+
